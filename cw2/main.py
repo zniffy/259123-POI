3940754cd00ec053992f050844b7f78ec07bed46
zniffy <zniffy@op.pl>
2025-04-27 01:45:45 +0200

    test ransac 1.0

--- cw2/main.py
+++ cw2/main.py
@@ -42,7 +42,7 @@ ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 ax.set_title('3D KMeans Clustering')
 ax.legend()
-plt.show()
+plt.show(block=False)
 # END of KMeans
 
 # START of RANSAC
@@ -62,22 +62,15 @@ red_plane_arr = np.array(red_plane)
 blue_plane_arr = np.array(blue_plane)
 green_plane_arr = np.array(green_plane)
 
-idx_random_3p_red = np.random.choice(red_plane_arr.shape[0], size=3, replace=False)
-idx_random_3p_blue = np.random.choice(blue_plane_arr.shape[0], size=3, replace=False)
-idx_random_3p_green = np.random.choice(green_plane_arr.shape[0], size=3, replace=False)
 
-random_3p_red = red_plane_arr[idx_random_3p_red]
-random_3p_blue = blue_plane_arr[idx_random_3p_blue]
-random_3p_green = green_plane_arr[idx_random_3p_green]
-
-#print(red_plane_arr)
-#print(random_3p_red)
-
-def ransac_method(num_of_inliers, treshold, max_attempts, arr):
+def ransac_method(num_of_inliers, treshold, max_attempts, arr, color=""):
     attempts = 0
-    inliers = 0
+    best_inliers_count = 0
+    best_attempt = None
+    best_normal = None
+    num_of_inliers_found = False
 
-    while attempts <=  max_attempts:
+    while attempts <= max_attempts:
         idx_arr = np.random.choice(arr.shape[0], size=3, replace=False)
         random_3p = arr[idx_arr]
         A_p = random_3p[0]
@@ -87,26 +80,58 @@ def ransac_method(num_of_inliers, treshold, max_attempts, arr):
         vec_A = A_p - C_p
         vec_B = B_p - C_p
 
-        uvec_A = vec_A/np.linalg.norm(vec_A)
-        uvec_B = vec_B/np.linalg.norm(vec_B)
-        uvec_C = np.cross(uvec_A, uvec_B)
+        normal_vector = np.cross(vec_A, vec_B)
+        uvec_C = normal_vector / np.linalg.norm(normal_vector)
 
-        
-        D = -np.sum(np.multiply(uvec_C, C_p))
-        distance_all_p = (uvec_C*arr + D)/np.linalg.norm(uvec_C)
-        inliers_arr = np.where(np.abs(distance_all_p <= treshold))[0]
-        inliers = len(inliers_arr)
+        D = -np.dot(uvec_C, C_p)
+        distances = np.abs(np.dot(uvec_C, arr.T) + D)
+        inliers_arr = np.where(distances <= treshold)[0]
+        inliers_count = len(inliers_arr)
 
         attempts += 1
 
-        if inliers >= num_of_inliers:
-            print("found answer with:", inliers, "inliers, within:", attempts, "attempts")
-            break;
+        if inliers_count > best_inliers_count:
+            best_inliers_count = inliers_count
+            best_normal = uvec_C
+            best_attempt = attempts
 
-    if attempts > max_attempts:
-        print("No answer")
-        print("Managed to find", inliers, "inliers")
+        if best_inliers_count >= num_of_inliers:
+            num_of_inliers_found = True
+
         
+        
+    if num_of_inliers_found:
+        print("Found plane with:", best_inliers_count, "inliers, within:", best_attempt, "attempts")
+        print("Best normal vector:", best_normal)
+        if color == "r":
+            if (best_normal[2] == 1) or (best_normal[2] == -1):
+                print("Red plane is horizontal")
+            elif (best_normal[0] == 1) or (best_normal[0] == -1) or (best_normal[1] == 1) or (best_normal[1] == -1):
+                print("Red plane is vertical")
+            else:
+                print("Red object is not horizontal nor vertical")
+        elif color == "b":
+            if (best_normal[2] == 1) or (best_normal[2] == -1):
+                print("Blue plane is horizontal")
+            elif (best_normal[0] == 1) or (best_normal[0] == -1) or (best_normal[1] == 1) or (best_normal[1] == -1):
+                print("Blue plane is vertical")
+            else:
+                print("Blue object is not horizontal nor vertical")
+        elif color == "g":
+            if (best_normal[2] == 1) or (best_normal[2] == -1):
+                print("Green plane is horizontal")
+            elif (best_normal[0] == 1) or (best_normal[0] == -1) or (best_normal[1] == 1) or (best_normal[1] == -1):
+                print("Green plane is vertical")
+            else:
+                print("Green object is not horizontal nor vertical")
+        print("######################################################")
+    else:
+        print("No sufficiently good plane found within", max_attempts, "attempts.")
+        print("Maximum inliers found:", best_inliers_count)
+        print("######################################################")
 
 
-ransac_method(300, 0.0001, 100, red_plane_arr)
+ransac_method(20, 1, 100, red_plane_arr, "r")
+ransac_method(20, 1, 100, blue_plane_arr, "b")
+ransac_method(20, 1, 100, green_plane_arr, "g")
+input() # keeps the plot on and lets the program to execute (press Enter in terminal to terminate)
